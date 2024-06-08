95febbd55fce1f8d99f562095b4d2bd226ac14e9
Muhammad Fachri Rasyidi <97487531+Frasydi@users.noreply.github.com>
2024-06-08 11:02:11 +0000

    init db

--- frontendvoting/src/router/home/index.tsx
+++ frontendvoting/src/router/home/index.tsx
@@ -1,5 +1,18 @@
+import { useEffect } from "react"
 import style from "./indes.module.css"
 export default function Home() {
+
+    async function fetching() {
+        const feting = await fetch("/api")
+        console.log(feting.status)
+        const texting = await feting.text()
+        console.log(texting) 
+    }
+
+    useEffect(() => {
+        fetching()
+    }, [])
+
     return(
         <div className={style.container}>
             Test
